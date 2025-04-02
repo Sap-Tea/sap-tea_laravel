2d5dce0d554e99e0fc54eb26f3be5f5e23047604
Sap-Tea <marcosbarroso.info@gmail.com>
2025-04-02 18:00:38 -0300

    inserir eixo Models

---
+++ app/Http/Controllers/InseriEixoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\EixoComportamento;
+use App\Models\EixoComunicacaoLinguagem;
+use App\Models\EixoInteracaoSocEmocional;
+
+use Illuminate\Http\Request;
+
+class InseriEixoController extends Controller
+{
+    
+}
